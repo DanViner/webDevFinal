b9b372e06b108c8928e700bf74b20c6b352e313b
Daniel Viner <33025424+DanViner@users.noreply.github.com>
2018-03-21 10:49:29 +0000

    Update NO2.php
    Slight formatting changes to aid readability.

--- php/NO2.php
+++ php/NO2.php
@@ -26,68 +26,68 @@ $input = array("brislington.xml", "fishponds.xml", "parson_st.xml",
     "rupert_st.xml", "wells_rd.xml", "newfoundland_way.xml");
 $output = array("brislington_NO2.xml", "fishponds_NO2.xml", "parson_st_NO2.xml", 
     "rupert_st_NO2.xml", "wells_rd_NO2.xml", "newfoundland_way_NO2.xml");
-//six files, so we loop six times.
+//Loop 6 times to cover all outputs
 for($i = 0; $i < count($output); $i++ ){
     //initialise reader, define file to read.
-    $reader = new reader();
-    $reader->open("../xml/orig/" . $input[$i]);
+    $xmlReader = new reader();
+    $xmlReader->open("../xml/orig/" . $input[$i]);
     
     //initialise writer, allocate memory.
-    $writer = new writer();
-    $writer->openMemory();
+    $xmlWriter = new writer();
+    $xmlWriter->openMemory();
     
     //create "header"
-    $writer->startDocument("1.0");
-    $writer->setIndent("2");
-    $writer->startElement("data");
-    $writer->writeAttribute("type", "nitrogen dioxide");
+    $xmlWriter->startDocument("1.0");
+    $xmlWriter->setIndent("2");
+    $xmlWriter->startElement("data");
+    $xmlWriter->writeAttribute("type", "nitrogen dioxide");
     /*
      * <?xml version="1.0"?>
      * <data type="nitrogen dioxide">
      */
     //set reader to the first row
-    while($reader->read() && $reader->name !=="row");
+    while($xmlReader->read() && $reader->name !=="row");
     
     $location = true;
     $doc = new DOMDocument;
     
-    while($reader->name === "row"){
-        $element = simplexml_import_dom($doc->importNode($reader->expand(), 
+    while($xmlReader->name === "row"){
+        $element = simplexml_import_dom($doc->importNode($xmlReader->expand(), 
                 true));
         
         //we only need the location value once per file, only entered on the 
         //   first pass.
         //<location long="--" lat="--" id="-----">
         if($location){
-            $writer->startElement("location");
-            $writer->writeAttribute("id", $element->desc->attributes()->val);
-            $writer->writeAttribute("lat", $element->lat->attributes()->val);
-            $writer->writeAttribute("long", $element->long->attributes()
+            $xmlWriter->startElement("location");
+            $xmlWriter->writeAttribute("id", $element->desc->attributes()->val);
+            $xmlWriter->writeAttribute("lat", $element->lat->attributes()->val);
+            $xmlWriter->writeAttribute("long", $element->long->attributes()
                     ->val);
             $location = false;
         }
         
         //write bulk of data
         //<reading val="--" time="--:--:--" date="--/--/----"/>
-        $writer->startElement("reading");
-        $writer->writeAttribute("date", $element->date->attributes()->val);
-        $writer->writeAttribute("time", $element->time->attributes()->val);
-        $writer->writeAttribute("val", $element->no2->attributes()->val);
-        $writer->endElement();
-        $reader->next("row");
+        $xmlWriter->startElement("reading");
+        $xmlWriter->writeAttribute("date", $element->date->attributes()->val);
+        $xmlWriter->writeAttribute("time", $element->time->attributes()->val);
+        $xmlWriter->writeAttribute("val", $element->no2->attributes()->val);
+        $xmlWriter->endElement();
+        $xmlReader->next("row");
     }
     /*
      * </location>
      * </data>
      */
-    $writer->endElement();
-    $writer->endElement();
-    $writer->endDocument();
+    $xlmWriter->endElement();
+    $xmlWriter->endElement();
+    $xmlWriter->endDocument();
     
     //create files
-    file_put_contents("../xml/no2/" . $output[$i], $writer->flush(true));
+    file_put_contents("../xml/no2/" . $output[$i], $xmlWriter->flush(true));
     
     //not necessary, but good practice.
-    $reader->close();
+    $xmlReader->close();
 }
 ?>
